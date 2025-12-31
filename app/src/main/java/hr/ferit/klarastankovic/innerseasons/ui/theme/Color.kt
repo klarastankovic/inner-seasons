eec7e434f22f6dd47fba4557bf3bbdae7c26dee4
klarastankovic <klara.stankovic9@gmail.com>
2025-12-31 13:47:45 +0100

    Change colors in Color and Theme

--- app/src/main/java/hr/ferit/klarastankovic/innerseasons/ui/theme/Color.kt
+++ app/src/main/java/hr/ferit/klarastankovic/innerseasons/ui/theme/Color.kt
@@ -9,31 +9,9 @@ val SpringGreen = Color(0xFF9EDB86)
 val SummerYellow = Color(0xFFFFE43D)
 val AutumnOrange = Color(0xFFDE871D)
 
-// Neutral Colors
 val White = Color(0xFFFFFFFF)
 val Black = Color(0xFF000000)
-val LightGray = Color(0xFFF5F5F5)
-val MediumGray = Color(0xFFE0E0E0)
-val DarkGray = Color(0xFF757575)
 
-// Background
-val BackgroundLight = Color(0xFFFAFAFA)
-val BackgroundDark = Color(0xFF121212)
-
-// Text Colors
 val TextPrimary = Color(0xFF212121)
 val TextSecondary = Color(0xFF757575)
 val TextHint = Color(0xFFBDBDBD)
-
-// Mood Colors (for mood selector)
-val MoodVeryBad = Color(0xFFE53935)    // Red
-val MoodBad = Color(0xFFFF9800)        // Orange
-val MoodNeutral = Color(0xFFFDD835)    // Yellow
-val MoodGood = Color(0xFF8BC34A)       // Light Green
-val MoodVeryGood = Color(0xFF4CAF50)   // Green
-
-// Pain Level Colors
-val PainNone = Color(0xFF4CAF50)       // Green
-val PainMild = Color(0xFFFDD835)       // Yellow
-val PainModerate = Color(0xFFFF9800)   // Orange
-val PainSevere = Color(0xFFE53935)     // Red
