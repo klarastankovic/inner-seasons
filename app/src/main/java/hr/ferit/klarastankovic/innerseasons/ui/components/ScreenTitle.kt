bea508b0f016ebec122abad2cf168c4b08d6edc3
klarastankovic <klara.stankovic9@gmail.com>
2026-01-04 20:32:24 +0100

    Add Screen Title component

---
+++ app/src/main/java/hr/ferit/klarastankovic/innerseasons/ui/components/ScreenTitle.kt
@@ -0,0 +1,28 @@
+package hr.ferit.klarastankovic.innerseasons.ui.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import hr.ferit.klarastankovic.innerseasons.ui.theme.Black
+
+@Composable
+fun ScreenTitle(
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = title,
+        style = TextStyle(
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 32.sp
+        ),
+        color = Black,
+        modifier = modifier.padding(24.dp)
+    )
+}
