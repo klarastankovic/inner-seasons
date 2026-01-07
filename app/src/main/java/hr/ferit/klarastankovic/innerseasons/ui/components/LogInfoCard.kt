4581a9c6bd936441afbfdf715e8352f8607ee509
klarastankovic <klara.stankovic9@gmail.com>
2026-01-07 22:07:13 +0100

    Add Log Info Card component

---
+++ app/src/main/java/hr/ferit/klarastankovic/innerseasons/ui/components/LogInfoCard.kt
@@ -0,0 +1,78 @@
+package hr.ferit.klarastankovic.innerseasons.ui.components
+
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import hr.ferit.klarastankovic.innerseasons.ui.theme.PrimaryPink
+import hr.ferit.klarastankovic.innerseasons.ui.theme.TextSecondary
+import hr.ferit.klarastankovic.innerseasons.ui.theme.White
+
+@Composable
+fun LogInfoCard(
+    label: String,
+    value: String,
+    modifier: Modifier = Modifier,
+    onClick: (() -> Unit)? = null,
+    showPlusIcon: Boolean = false
+) {
+    Card(
+        modifier = modifier.width(160.dp),
+        shape = MaterialTheme.shapes.small,
+        colors = CardDefaults.cardColors(
+            containerColor = PrimaryPink
+        ),
+        elevation = CardDefaults.cardElevation(defaultElevation = 1.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(horizontal = 20.dp, vertical = 20.dp)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = label,
+                    fontSize = 16.sp,
+                    color = White
+                )
+
+                if (showPlusIcon) {
+                    Spacer(modifier = Modifier.width(4.dp))
+
+                    Text(
+                        text = "+100ml",
+                        fontSize = 12.sp,
+                        color = White
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = value,
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Medium,
+                color = White,
+                textAlign = TextAlign.Right
+            )
+        }
+    }
+}
